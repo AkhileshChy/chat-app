e5412bfb018844d4e2a6def253268d7190ba790c
Akhilesh <choudharyakhilesh2003@gmail.com>
2024-06-23 15:34:13 +0530

    Add routes+controllers

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+
+function App() {
+
+  return (
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <SignUp />
+    </div>
+  )
+}
+
+export default App
